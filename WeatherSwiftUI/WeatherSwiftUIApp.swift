5edc29c7e15d9744ff4c3e8fbb399332fa99038e
Peggy Hunt <phunt1000@gmail.com>
2022-07-13 13:40:06 -0500

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Peggy Hunt on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
